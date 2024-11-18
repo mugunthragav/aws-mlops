5b88162d1df6f71a663ac4878c2f4162b5e0e400
mugunthragav <mugunthragav466@gmail.com>
2024-11-18 13:46:08 +0530

    Update register_promote.py

--- register_promote.py
+++ register_promote.py
@@ -1,25 +1,38 @@
 import mlflow
 import boto3
 import joblib
+from mlflow.exceptions import RestException
 
 # Initialize S3 client
 s3 = boto3.client('s3')
 model_bucket = 'model-bucket-house-model'
 
 def register_and_promote():
-    # Download best model
+    # Download best model from S3
     s3.download_file(model_bucket, 'best_model.pkl', 'best_model.pkl')
     model = joblib.load('best_model.pkl')
 
+    # Initialize MLflow client
     mlflow.set_tracking_uri("http://ec2-100-24-6-128.compute-1.amazonaws.com:5000")
     client = mlflow.tracking.MlflowClient()
 
-    # Register model
     model_name = "HousePricePrediction01"
-    mlflow.sklearn.log_model(model, model_name)
 
-    # Transition to production
+    # Register the model if it doesn't exist
+    try:
+        client.get_registered_model(model_name)
+        print(f"Model {model_name} is already registered.")
+    except RestException as e:
+        if "RESOURCE_DOES_NOT_EXIST" in str(e):
+            print(f"Model {model_name} is not registered. Registering now.")
+            mlflow.sklearn.log_model(model, model_name, registered_model_name=model_name)
+        else:
+            raise
+
+    # Get the latest version of the model
     latest_version = client.get_latest_versions(model_name, stages=["None"])[0].version
+
+    # Transition model to production
     client.transition_model_version_stage(
         name=model_name,
         version=latest_version,
